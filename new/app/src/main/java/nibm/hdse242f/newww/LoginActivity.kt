d784d33eb15f3571029ce4be60b047ed6ba73ce3
chetha03 <chethanaiduwari@gmail.com>
2025-11-01 23:20:47 +0530

    login
    done

---
+++ new/app/src/main/java/nibm/hdse242f/newww/LoginActivity.kt
@@ -0,0 +1,19 @@
+package nibm.hdse242f.newww
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
